b57b4cbb74a4e9eb534db1e17ac68fe5c397dda5
qq <895462507@qq.com>
2020-01-07 15:54:06 +0800

    123

---
+++ GOOD/Chapter13/src/main/java/com/course/Application.java
@@ -0,0 +1,27 @@
+package com.course;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PreDestroy;
+
+@EnableScheduling
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+
+}
